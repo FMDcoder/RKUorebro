cf1cbb577215ee782d54de08cfd103a9a944bb56
FMDcoder <76693953+FMDcoder@users.noreply.github.com>
2023-09-10 18:45:19 +0200

    a

--- src/event.js
+++ src/event.js
@@ -13,13 +13,14 @@ const welcome = (member) => {
         const embedMsg = new EmbedBuilder()
             .setTitle("Välkommen Till RKU Örebros Discord Server!")
             .setDescription(
-                "Välkommen kamrat "+member.user.username+"! "+
+                "Välkommen kamrat "+member.user.username+"!\n\n"+
                 "Var vänligen och läs <#1148623875295416350> och sedan verifiera dig i "+
                 "<#1148624214379745280> genom att svara på frågorna i "+
-                "<#1148630219977138237>! Så kommer vi granska dig snart!"
+                "<#1148630219977138237>! Så kommer vi granska dig snart!\n\n"+
+                "Kamratligen RKU Bot"
             )
             .setColor(0xad0003)
-            .setImage("https://github.com/FMDcoder/RKUorebro/blob/main/images/RKU.png")
+            .setImage("https://raw.githubusercontent.com/FMDcoder/RKUorebro/main/images/RKU.png?token=GHSAT0AAAAAACHMT3QH4UML6KPN4SVXYBUEZH56EKQ")
         
         member.guild.channels.cache.get("1148629886911664158").send({embeds: [embedMsg]})
     }, 1000);
