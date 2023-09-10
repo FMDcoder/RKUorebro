09822e5cca34ef84972d7f8fb50c0a9408733817
FMDcoder <76693953+FMDcoder@users.noreply.github.com>
2023-09-10 18:33:21 +0200

    a

--- src/event.js
+++ src/event.js
@@ -6,19 +6,22 @@ const welcome = (member) => {
     let role = member.guild.roles.cache.find(role => role.name === "Overifierad");
     member.roles.add(role);
 
-    let userId = member.toString();
+    setTimeout(() => {
+        let userId = member.toString();
+        member.guild.channels.cache.get("1148629886911664158").send(userId);
 
-    const embedMsg = new EmbedBuilder()
-        .setTitle("Välkommen Till RKU Örebros Discord Server!")
-        .setDescription(
-            "Välkommen kamrat "+userId+"! "+
-            "Var vänligen och läs <#1148623875295416350> och sedan verifiera dig i "+
-            "<#1148624214379745280> genom att svara på frågorna i "+
-            "<#1148630219977138237>! Så kommer vi granska dig snart!"
-        )
-        .setColor(0xad0003)
-    
-    member.guild.channels.cache.get("1148629886911664158").send({embeds: [embedMsg]})
+        const embedMsg = new EmbedBuilder()
+            .setTitle("Välkommen Till RKU Örebros Discord Server!")
+            .setDescription(
+                "Välkommen kamrat "+member.username+"! "+
+                "Var vänligen och läs <#1148623875295416350> och sedan verifiera dig i "+
+                "<#1148624214379745280> genom att svara på frågorna i "+
+                "<#1148630219977138237>! Så kommer vi granska dig snart!"
+            )
+            .setColor(0xad0003)
+        
+        member.guild.channels.cache.get("1148629886911664158").send({embeds: [embedMsg]})
+    }, 1000);
 }
 
 module.exports = {welcome}
